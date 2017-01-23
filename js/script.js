adf99e435fa12cdfd3f5358c82d27ce7a6386c31
jate-web <jate.web@gmail.com>
2017-01-23 17:35:07 +0300

    add several blocks

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuery(function($) {
+    $(document).ready(function () {
+        $('.slick_slider').slick({
+            autoplay: true,
+            dots: true,
+            arrows: false,
+        });
+    });
+
+
+});
